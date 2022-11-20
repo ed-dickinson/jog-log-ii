e562b52018734a5b890456d8f8e29de4c7b091f9
ed-dickinson <edwardejd@gmail.com>
2022-11-20 08:00:46 +0000

    strava activity titling

--- src/components/Writer.js
+++ src/components/Writer.js
@@ -1,13 +1,23 @@
-import React, {useState} from 'react'
+import React, {useState, useEffect} from 'react'
 
 const Writer = ({writerOpen, setWriterOpen, stravaActivity}) => {
 
   const [runDescription, setRunDescription] = useState('');
+  const [runTitle, setRunTitle] = useState(stravaActivity ? stravaActivity.name : '')
+
+  useEffect(()=>{
+    setRunTitle(stravaActivity ? stravaActivity.name : '')
+  },[stravaActivity])
 
   return (
     <aside className={"Writer" + (writerOpen ? ' show' : ' hide')}>
     <div className="Card" >
-      {stravaActivity && <div className="StravaTitle">{stravaActivity.name}</div>}
+
+      <input
+        className="RunTitle"
+        value={runTitle}
+        onChange={({target}) => setRunTitle(target.value)}
+      ></input>
 
       <textarea
         type="textarea" name="description"
